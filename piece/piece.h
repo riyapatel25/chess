0cc56ac2e97ac8eed5d86463876a2f89b35892ed
riyapatel25 <riyap7521@gmail.com>
2023-07-24 21:28:41 -0400

    group changes

--- piece/piece.h
+++ piece/piece.h
@@ -10,7 +10,7 @@ protected:
     // int newCol;
 
 public:
-    virtual void playerMove (int row, int col, int newRow, int newCol) = 0;
+    virtual bool playerMove (int row, int col, int newRow, int newCol) = 0;
     virtual bool canPlayerKill (int row, int col) = 0;
     bool isCheck (int row, int col, int newRow, int newCol);
     virtual ~Piece();
