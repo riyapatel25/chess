d6baec2758d9bba76929e0aa5103629921e41621
riyapatel25 <riyap7521@gmail.com>
2023-07-28 20:07:56 -0400

    finished board constructor

--- board.h
+++ board.h
@@ -7,24 +7,27 @@
 
 class Board {
     private:
-    Board(type1: string, type2: string, isSetup: bool, setupString: string);
-    static Board *currBoard;
+    Board(type1: string, type2: string, isSetup: bool, setupString: string); //Board Constructor
+    void setupBoard(defaultOrInput: bool); //helper for constructor 
     Score currScore;
-    bool turn;
+    bool turn; //keeps track of who's turn -> white (true), black (false)
     Player player1; //white player
     Player player2; //black player
 
+    protected:
+    Board *currBoard; 
+
 	public:
-	~Board();
+	~Board(); //destrcutor
+    void play(input: string); //function to make a move
+    bool isConfigurationValid(board: &Board const); //check is board is setup correctly -> helper for constructor
     vector<vector<Piece* >> getPieces();
     bool isCheckmate(turn: bool);
     bool getTurn();
     void setPiece(row: int, col: int);
     void setTurn(turn: bool);
-    void play(input: string);
     bool isTaken(row: int, col: int);
-    void displayErrorHnadling(message: string);
-    void setupBoard();
-    bool isConfigurationValid(board: &Board const); //check is board is setup correctly
+    void displayErrorHandling(message: string);
+   
 };
 #endif
